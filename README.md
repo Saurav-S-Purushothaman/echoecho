78d3c1ed1aa2ccc7d93efd49e419b9ec331c9dcc
Saurav S Purushothaman <saurav.purushothaman@unifize.com>
2024-08-03 16:05:32 +0530

    Configure echo server
    As a first step to the program, this commit will configure an echo
    server which serves an echo server in port 7007 of localhost

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # echoecho
+
+As a starting step, this will act as an echo server that accepts just
+one client.
+
+The default port exposed is - 127.0.0.1:7007
+
+## Getting Started
+
+To start the program, clone this repository and run the below shell
+command
+
+``` shell
+$ clj -M -m echoecho.echo
+```
+
+Connect to the server using any client.
+Example using `telnet client`
+
+``` shell
+$ telnet localhost 7007
+```
+
+All send messages are echoed
+
+``` shell
+$ telnet localhost 7007
+]
+Trying ::1...
+Connected to localhost.
+Escape character is '^]'.
+this
+this
+that
+that
+with
+with
+more
+more
+not
+not
+chat
+chat
+```
---
+++ src/echoecho/echo.clj
@@ -0,0 +1,54 @@
+(ns echoecho.echo
+  "This is will be a temporary main file"
+  (:require [clojure.java.io :as io])
+  (:import [java.net ServerSocket])
+  (:gen-class))
+
+(defn receive
+  "Read a line of textual data from the given socket"
+  [socket]
+  ;; NOTE: There is no need to flush the reader because reader is not
+  ;;       using a buffered stream to read from the socket instead it
+  ;;       is read directly from the socket. Moreover readLine is a
+  ;;       blocking method.
+  (.readLine (io/reader socket)))
+
+(defn send
+  "Sends the given textual data to the given socket"
+  [socket text]
+  ;; NOTE: We need to flush the writer, because the writer uses
+  ;;       BufferedStream for efficiency. Flush will make sure that
+  ;;       all the data will be send back to the client instead of
+  ;;       being buffered into the writer instance
+  (let [writer (io/writer socket)]
+    (.write writer text)
+    (.flush writer)))
+
+(defn echo
+  "Returns the text by adding new line"
+  [arg]
+  (str arg "\n"))
+
+(defn serve
+  "Handles creating an instance of ServerSocket on a particular port.
+  Takes a handler function, which will be used to process the incoming
+  request and determine a response message"
+  [port handler]
+  (let [server-socket (ServerSocket. port)
+        ;; Calls server-sockets accept method. Blocks until
+        ;; session is established. Returs Socket instance upon
+        ;; successful connection of client
+        socket (.accept server-socket)
+        msg-in (atom nil)
+        msg-out (atom nil)]
+    (loop []
+      (when-not (.isClosed socket)
+        (reset! msg-in (receive socket))
+        (reset! msg-out (handler @msg-in))
+        (send socket @msg-out))
+      (recur))))
+
+(defn -main
+  "The entry point of our application"
+  [& _]
+  (serve 7007 echo))
