eb059c8b72925a49f8be388f1f723a992b5b854f
Saurav S Purushothaman <saurav.purushothaman@unifize.com>
2024-08-05 23:43:26 +0530

    Make the server asynchronously to multiple client
    This still doesn't work. But it's a partial solution anyway

--- src/echoecho/echo.clj
+++ src/echoecho/echo.clj
@@ -1,13 +1,10 @@
 (ns echoecho.echo
-  (:import
-   [java.io
-    DataOutputStream
-    DataInputStream
-    BufferedReader
-    InputStreamReader]
-   [java.net
-    ServerSocket
-    Socket])
+  (:require [clojure.core.async :as a :refer [>!! <!! >! <!]])
+  (:import [java.io
+            DataOutputStream
+            DataInputStream]
+           [java.net
+            ServerSocket])
   (:gen-class))
 
 (defn echo
@@ -15,23 +12,36 @@
   [arg]
   (str arg "\n"))
 
+(defn create-async-socket [chan server-socket]
+  (a/thread (>!! chan (.accept server-socket))))
+
+(defn get-async-socket [chan]
+  (<!! chan))
+
 (defn server
   [port handler]
-  (let [server-socket (ServerSocket. port)
-        socket (.accept server-socket)
-        socket-input-stream (DataInputStream. (.getInputStream socket))
-        socket-output-stream (DataOutputStream. (.getOutputStream socket))
-        msg-in (atom nil)
-        msg-out (atom nil)]
-    ;; Type /quit to quit the connection.
-    (while (not= @msg-in  "/quit")
-      (reset! msg-in (.readLine socket-input-stream))
-      (reset! msg-out (handler @msg-in))
-      (.writeBytes socket-output-stream @msg-out)
-      (.flush socket-output-stream))
-    (.close socket-input-stream)
-    (.close socket)))
+  (while true
+    (let [chan (a/chan)
+          server-socket (ServerSocket. port)
+          _ (create-async-socket chan server-socket)
+          socket (get-async-socket chan)
+          socket-input-stream (DataInputStream. (.getInputStream socket))
+          socket-output-stream (DataOutputStream. (.getOutputStream socket))
+          msg-in (atom nil)
+          msg-out (atom nil)]
+      (create-async-socket chan server-socket)
+      ;; Type /quit to quit the connection.
+      (while (not= @msg-in  "/quit")
+        (reset! msg-in (.readLine socket-input-stream))
+        (reset! msg-out (handler @msg-in))
+        (.writeBytes socket-output-stream @msg-out)
+        (.flush socket-output-stream))
+      (.close socket-input-stream)
+      (.close socket))))
 
 (defn -main
   [& _]
   (server 7007 echo))
+
+;; TODO: Add for multiclient support
+;; What we really need to implement is non blocking io operation
