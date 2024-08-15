450e54ec190f8fc875bbb25a3852ec86e82fbcf5
Saurav S Purushothaman <saurav.purushothaman@unifize.com>
2024-08-15 20:25:38 +0530

    Implement server that connects to multiple client
    This server will connect to multiple client using a single thread.

--- src/echoecho/test_server.clj
+++ src/echoecho/test_server.clj
@@ -8,36 +8,86 @@
   (:require [taoensso.timbre :as timbre])
   (:gen-class))
 
+(defn get-socket-channel
+  [key]
+  (cast java.nio.channels.SocketChannel (.channel key)))
+
+(defn client-host
+  [client]
+  (doto client
+    (.getInetAddress)
+    (.getHostAddress)))
+
+(defn client-port
+  [client]
+  (.getPort client))
+
+(defn client-info
+  [client-socket]
+  (let [host-address (client-host client-socket)
+        port (client-port client-socket)
+        log {:host host-address
+             :port port}]
+    (timbre/info "Connected:" log)
+    (timbre/info "Type of client socket :" (type client-socket))))
+
+
+;; Storing all the clients in an atom to close it finally.
+(defonce clients-state (atom #{}))
 
 (defn start-server
   [port-number]
   (try
     (let [server-socket-channel (ServerSocketChannel/open)
           selector (Selector/open)
-          inet-address (InetSocketAddress. port-number)]
+          inet-address (InetSocketAddress. port-number)
+          clients #{}]
       (.configureBlocking server-socket-channel false)
       (.bind server-socket-channel inet-address)
-      ;; Register the server-socket-channel with the selector. It will,
-      ;; when a server socket channel has an Operation Accept method
-      ;; ready to be processed or polled, fire the selector
       (.register server-socket-channel selector (SelectionKey/OP_ACCEPT))
       (loop []
+        ;; this is blocking
         (if (zero? (.select selector))
           (recur)
           (do
             (doseq [key (.selectedKeys selector)]
-              ;; Just log the key right now, we will handle this later
-              (timbre/info "The server started")
-              (timbre/info key)
-              (if (instance? (.channel key) ServerSocketChannel)
-              ;; The key is only acceptable, if the key is pointing to
-              ;; an accept on a socket
-                (let [channel (.channel key)]
-                  (println (type channel)))
-              (if (.isAcceptable key)
-                true
-                false))
-            (doto selector (.selectedKeys) (.clear)))))))
+              (let [key-channel (.channel key)]
+                (timbre/info "Got Channel")
+                (timbre/info "The key is : " key)
+                (timbre/info "Type of key is " (class key))
+                (timbre/info "Type of key.channel is " (class key-channel))
+                ;; The key is only acceptable, if the key is pointing to
+                ;; an accept on a socket
+                (when (.isAcceptable key)
+                  (when (instance? key-channel ServerSocketChannel)
+                    ;; We will see if this let is necessary
+                    (let [channel (get-socket-channel key)
+                          ;; this is non blocking
+                          client (.accept channel)
+                          client-socket (.socket client)]
+                      (client-info client-socket)
+                      ;; Configure blocking for the socket and also
+                      ;; register the client on the same selector but
+                      ;; with read key
+                      (.configureBlocking client false)
+                      (.register selector (SelectionKey/OP_READ) client)
+                      (swap! clients-state conj client))
+                    true)))
+              (doto selector (.selectedKeys) (.clear)))))))
     (catch Exception e
       (timbre/error "Server open failed with Exception: " e)
-      (throw (RuntimeException. e)))))
+      (throw (RuntimeException. e)))
+    (finally
+      (try
+        (doseq [x @clients-state]
+          (.close x))
+        (catch Exception e
+          (timbre/error "Failed to close the connection with Exception" e)
+          (throw (RuntimeException. e)))))))
+
+(defn -main
+  [& args]
+  (start-server 7007))
+
+(comment
+  (start-server 7007))
