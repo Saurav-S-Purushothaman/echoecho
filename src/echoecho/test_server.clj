274c514781a25aafd2aac48c925e8e05fcb7e988
Saurav S Purushothaman <saurav.purushothaman@unifize.com>
2024-08-16 00:16:59 +0530

    Refactor test-server

--- src/echoecho/test_server.clj
+++ src/echoecho/test_server.clj
@@ -9,8 +9,8 @@
   (:gen-class))
 
 (defn get-socket-channel
-  [key]
-  (cast java.nio.channels.SocketChannel (.channel key)))
+  [key-channel]
+  (cast java.nio.channels.ServerSocketChannel key-channel))
 
 (defn client-host
   [client]
@@ -31,6 +31,33 @@
     (timbre/info "Connected:" log)
     (timbre/info "Type of client socket :" (type client-socket))))
 
+(defn initialize-server-socket!
+  "Sets up the server socket channel for non-blocking operations, binds
+  it to the given address, and registers it with the selector for
+  accepting connections."
+  [server-socket-channel inet-address selector]
+  (.configureBlocking server-socket-channel false)
+  (.bind server-socket-channel inet-address)
+  (.register server-socket-channel selector (SelectionKey/OP_ACCEPT)))
+
+(defn initialize-client-socket!
+  "Sets up the server socket channel for non-blocking operations,
+  registers it with the selector for accepting connections and adds the
+  client to clients-state atom"
+  [client selector clients-state]
+  (.configureBlocking client false)
+  (.register client selector (SelectionKey/OP_READ))
+  (swap! clients-state conj client))
+
+(defn close-client-socket!
+  [clients-state]
+  (try
+    (doseq [x @clients-state]
+      (.close x))
+    (catch Exception e
+      (timbre/error "Failed to close the connection with Exception" e)
+      (throw (RuntimeException. e)))))
+
 
 ;; Storing all the clients in an atom to close it finally.
 (defonce clients-state (atom #{}))
@@ -40,50 +67,30 @@
   (try
     (let [server-socket-channel (ServerSocketChannel/open)
           selector (Selector/open)
-          inet-address (InetSocketAddress. port-number)
-          clients #{}]
-      (.configureBlocking server-socket-channel false)
-      (.bind server-socket-channel inet-address)
-      (.register server-socket-channel selector (SelectionKey/OP_ACCEPT))
+          inet-address (InetSocketAddress. port-number)]
+      (initialize-server-socket! server-socket-channel inet-address selector)
       (loop []
         ;; this is blocking
         (if (zero? (.select selector))
-          (recur)
           (do
-            (doseq [key (.selectedKeys selector)]
-              (let [key-channel (.channel key)]
-                (timbre/info "Got Channel")
-                (timbre/info "The key is : " key)
-                (timbre/info "Type of key is " (class key))
-                (timbre/info "Type of key.channel is " (class key-channel))
-                ;; The key is only acceptable, if the key is pointing to
-                ;; an accept on a socket
-                (when (.isAcceptable key)
-                  (when (instance? key-channel ServerSocketChannel)
-                    ;; We will see if this let is necessary
-                    (let [channel (get-socket-channel key)
-                          ;; this is non blocking
-                          client (.accept channel)
-                          client-socket (.socket client)]
-                      (client-info client-socket)
-                      ;; Configure blocking for the socket and also
-                      ;; register the client on the same selector but
-                      ;; with read key
-                      (.configureBlocking client false)
-                      (.register selector (SelectionKey/OP_READ) client)
-                      (swap! clients-state conj client))
-                    true)))
-              (doto selector (.selectedKeys) (.clear)))))))
+            (println "Closing the system")
+            (System/exit 0))
+          (doseq [key (.selectedKeys selector)]
+            (let [key-channel (.channel key)]
+              (when (.isAcceptable key)
+                (let [;; this is non blocking
+                      key-channel' (get-socket-channel key-channel)
+                      client (.accept key-channel')]
+                  ;; Register the client on the same selector but
+                  ;; with read key
+                  (initialize-client-socket! client selector clients-state)
+                  (.register client selector (SelectionKey/OP_READ))
+                  (swap! clients-state conj client))))))
+        (recur)))
     (catch Exception e
       (timbre/error "Server open failed with Exception: " e)
       (throw (RuntimeException. e)))
-    (finally
-      (try
-        (doseq [x @clients-state]
-          (.close x))
-        (catch Exception e
-          (timbre/error "Failed to close the connection with Exception" e)
-          (throw (RuntimeException. e)))))))
+    (finally (close-client-socket! clients-state))))
 
 (defn -main
   [& args]
